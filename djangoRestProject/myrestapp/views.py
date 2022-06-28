428610266c8b70262bb4f45dbb9a1cb1aacba979
imsanjeev1 <kumar.sanjeev84@gmail.com>
2022-06-28 18:02:43 +0530

    Api added and models created

--- djangoRestProject/myrestapp/views.py
+++ djangoRestProject/myrestapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
+@api_view(["GET","POST","PATCH"])
+def home(request):
+    if request.method =="GET":
+        return Response({'message':"DjangoRest Get Api working !","status":200})
+    elif request.method =="POST":
+        return Response({'message':"DjangoRest POST Api working !","status":200})
+    elif request.method =="PATCH":
+        return Response({'message':"DjangoRest PATCH Api working !","status":200})
+    else:
+        return Response({'message':"Invalid Method not working !","status":400})
